ad069c6babd03704b48e29528fcfba86bdef4450
Roman Grishin <roman.grishin.me@gmail.com>
2024-04-03 10:09:10 +0500

    fix: checkbox

--- src/components/CheckboxElement.jsx
+++ src/components/CheckboxElement.jsx
@@ -4,23 +4,24 @@ import { Controller } from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 
 const checkboxContainer = cx(
-  "flex gap-16px items-center mt-auto",
-  "text-lg leading-lg text-ui-gray-500"
+  'flex gap-16px items-center mt-auto',
 );
 
-const labelClassName = cx(
-  'flex justify-start items-start mt-auto'
+const containerClassName = cx(
+  'flex justify-start items-start mt-auto',
+  'text-lg leading-lg text-ui-gray-500'
 )
 
-const containerClassName = cx(
+const checkboxClassname = cx(
   'flex flex-shrink-0 justify-center items-center',
   'w-6 h-6 mr-2',
-  'bg-white border-[1px] border-ui-gray-200 rounded-8px focus-within:border-ui-blue-600'
+  'bg-white border-[1px] border-ui-gray-200 rounded-8px checked:border-ui-blue-600'
 
 )
 
 const inputClassName = cx(
-  'opacity-0 absolute'
+  'opacity-0 absolute',
+  'w-6 h-6',
 )
 
 const errorClassName = cx(
@@ -39,7 +40,6 @@ const CheckboxElement = ({ name, label, control, register, errors }) => {
         className={checkboxContainer}
         rules={{ required: true }}
         render={() => (
-          <label className={labelClassName}>
           <div className={containerClassName}>
             <input
               type="checkbox"
@@ -48,23 +48,24 @@ const CheckboxElement = ({ name, label, control, register, errors }) => {
               className={inputClassName}
               { ...register(name) }
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              className="hidden pointer-events-none"
-            >
-              <path
-                d="M2.25 8L5.01193 11.5511C5.39578 12.0446 6.13331 12.0688 6.54869 11.6015L13.75 3.5"
-                stroke="#4F46E5"
-                strokeWidth="1.5"
-              />
-            </svg>
+            <div className={checkboxClassname}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                className='hidden text-blue-600 pointer-events-none'
+              >
+                <path
+                  d="M2.25 8L5.01193 11.5511C5.39578 12.0446 6.13331 12.0688 6.54869 11.6015L13.75 3.5"
+                  stroke="#4F46E5"
+                  strokeWidth="1.5"
+                />
+              </svg>
+            </div>
+            <label htmlFor={name} className='select-none'>{label}</label>
           </div>
-          <div>{label}</div>
-        </label>
         )}
       />
       <ErrorMessage
